f20aa448eee8c8314083fe0b71cdfb8943591cb9
4RY4P5 <47707526+4RY4P5@users.noreply.github.com>
2020-12-19 20:21:12 +0530

    Add files via upload

---
+++ TRUyum/truyumModel/src/com/cognizant/truyum/util/DateUtil.java
@@ -0,0 +1,15 @@
+package com.cognizant.truyum.util;
+
+import java.util.Date;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
+public class DateUtil {
+
+	public static Date convertToDate(String date) throws ParseException {
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		Date launchDate=sdf.parse(date);
+		return launchDate;
+		
+	}
+}
